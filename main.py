dea2138e438e5f11b8283a974c40fe830fb66dad
Jarek Majka <vessekheal@gmail.com>
2021-09-12 20:38:14 +0200

    Header option added

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-def csv_reader():
+def csv_reader(header=False, separator=','):
+    csv_data_list = []
     with open('csv_file_for_reading.csv', 'r') as csv_file:
-        csv_data_list = csv_file.readlines()
+        for line in csv_file:
+            csv_data_list.append(line.split(separator))
+    header = csv_data_list[0]
 
-    print(csv_data_list)
 
 
 if __name__ == '__main__':
-    csv_reader()
+    first_line_as_header = True
+    separate = ','
+    csv_reader(first_line_as_header, separate)
